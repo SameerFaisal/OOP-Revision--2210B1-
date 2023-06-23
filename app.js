3731acf28c251d9dcd4cd95bdea3978fce599930
SameerFaisal <sameerfaisal46@gmail.com>
2023-06-23 12:32:04 +0500

    23-06-2023

--- app.js
+++ app.js
@@ -45,37 +45,153 @@
 // v2.displayDetails()
 // v1.displayDetails()
 
-class Rectangle{
-    length
-    breadth
-    name
-    constructor(name,len,width){
-this.name=name
-this.length=len
-this.breadth= width
-    }
-    area(){
-        console.log('Area of Rectangle is: '+(this.length*this.breadth))
-    }
-    perimeter(){
-        console.log('Perimeter of Rectangle is: '+(2*(parseFloat(this.length)+parseFloat(this.breadth))))
+// class Rectangle{
+//     length
+//     breadth
+//     name
+//     constructor(name,len,width){
+// this.name=name
+// this.length=len
+// this.breadth= width
+//     }
+//     area(){
+//         console.log('Area of Rectangle is: '+(this.length*this.breadth))
+//     }
+//     perimeter(){
+//         console.log('Perimeter of Rectangle is: '+(2*(parseFloat(this.length)+parseFloat(this.breadth))))
+//     }
+//     display(){
+//         console.log('Name of the rectangle is: '+this.name)
+//         console.log('Length of the rectangle is: '+this.length)
+//         console.log('Breadth of the rectangle is: '+this.breadth)
+
+//     }
+// }
+
+
+// console.log('Data for first rectangle')
+// const r1=new Rectangle('Rect1',5,10)
+// r1.display()
+// r1.area()
+// r1.perimeter()
+// console.log('Data for first rectangle')
+// const r2=new Rectangle('Rect2',10,20)
+// r2.display()
+// r2.area()
+// r2.perimeter()
+
+
+// class Person{
+//     balance=0
+//     income=[
+//         {
+//         description:'Salary',
+//         amount:45000
+//     },
+//     {
+//         description: 'Gift',
+//             amount:5000
+//     }
+// ]
+
+// expense=[
+//     {
+// description:'Clothes',
+// amount:15000
+// },{
+//     description:"Food",
+//     amount:2000
+// }]
+
+// viewIncome(){
+//     let totalIncome=0;
+//     for(let i=0;i<this.income.length;i++){
+//         totalIncome=totalIncome+this.income[i].amount
+//     }
+//  document.getElementById("output").innerHTML='Total Income is: '+totalIncome
+// }
+// viewExpense(){
+//     let totalExpense=0;
+//     for(let j=0;j<this.expense.length;j++){
+//         totalExpense=totalExpense+this.expense[j].amount
+//     }
+//  document.getElementById("output").innerHTML='Total Expense is: '+totalExpense
+// }
+// viewBalance(){
+//     let totalExpense=0;
+//     for(let j=0;j<this.expense.length;j++){
+//         totalExpense=totalExpense+this.expense[j].amount
+//     }
+//     let totalIncome=0;
+//     for(let i=0;i<this.income.length;i++){
+//         totalIncome=totalIncome+this.income[i].amount
+//     }
+//     this.balance=(this.balance+totalIncome)-totalExpense
+//     document.getElementById("output").innerHTML='Total Balance is: '+this.balance
+// }
+
+// addIncome(){
+//     let desc=prompt("Enter source of income")
+//     let am=+prompt("Enter amount of in rupees")
+//     this.income.push({description:desc,amount:am})
+//     alert(desc+' with amount '+am+' added successfully')
+// }
+
+// addExpense(){
+//     let desc=prompt("Enter expense type")
+//     let am=+prompt("Enter the cost")
+//     if(this.balance<am){
+//         alert("Your balance is insufficeint")
+//     }
+//     else if(this.balance>=am){
+//         this.expense.push({description:desc,amount:am})
+//         alert(desc+' with amount '+am+' added successfully')
+//     }
+// }
+// }
+
+// const p1=new Person()
+
+class Vehicle{
+
+name
+model
+constructor(name,model){
+    this.name=name
+    this.model=model
+}
+display(){
+    console.log(`The vehicle name is ${this.name} and ${this.model}`)
+}
+}
+
+class Car extends Vehicle{
+    noOfSeats
+    constructor(name,model,noOfSeats){
+        super(name,model)
+        this.noOfSeats=noOfSeats
     }
     display(){
-        console.log('Name of the rectangle is: '+this.name)
-        console.log('Length of the rectangle is: '+this.length)
-        console.log('Breadth of the rectangle is: '+this.breadth)
-
+        super.display()
+        console.log(`There are ${this.noOfSeats} seats`)
     }
 }
 
+class Bike extends Vehicle{
 
-console.log('Data for first rectangle')
-const r1=new Rectangle('Rect1',5,10)
-r1.display()
-r1.area()
-r1.perimeter()
-console.log('Data for first rectangle')
-const r2=new Rectangle('Rect2',10,20)
-r2.display()
-r2.area()
-r2.perimeter()
+    //Write your code here
+}
+
+class Truck extends Vehicle{
+
+    //Write your code here
+}
+
+
+
+
+// const c1=new Car()
+
+const c1=new Car('Mehran',2004,5)
+c1.display()
+
